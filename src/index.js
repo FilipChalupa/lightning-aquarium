c5ba499f13dd63012f42a77c1f8434cbe858e2cc
Filip Chalupa <chalupa.filip@gmail.com>
2021-09-26 12:22:05 +0200

    Add ngrok

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import dotenv from 'dotenv'
 import express from 'express'
 import http from 'http'
 import lnService from 'ln-service'
+import ngrok from 'ngrok'
 import { createWebSocketServer } from './createWebSocketServer.js'
 
 dotenv.config()
 
-const { CERT, INVOICE_MACAROON, SOCKET, PORT } = process.env
+const { CERT, INVOICE_MACAROON, SOCKET, PORT, NODE_ENV } = process.env
+const isDevelopment = NODE_ENV === 'development'
+const isProduction = !isDevelopment
 
 const { lnd } = lnService.authenticatedLndGrpc({
 	cert: CERT,
@@ -21,6 +24,12 @@ createWebSocketServer(server, lnd)
 
 app.use(express.static('public'))
 
-server.listen(PORT, () => {
-	console.log(`Server started on port ${server.address().port}`)
+server.listen(PORT, async () => {
+	const port = server.address().port
+	console.log(`Server started on port ${port}`)
+
+	if (isProduction) {
+		const publicUrl = await ngrok.connect({ addr: port })
+		console.log(`Server running public at ${publicUrl}`)
+	}
 })
